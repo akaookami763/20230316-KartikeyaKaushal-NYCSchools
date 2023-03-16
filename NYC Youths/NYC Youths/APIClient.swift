d037f65c78d0d01e8ab7b9d4d1e15aebe7bd7c50
118-sethshadow <kai@Kartikeyas-MacBook-Pro.local>
2023-03-16 16:05:16 -0500

    First init.  No UI yet

---
+++ NYC Youths/NYC Youths/APIClient.swift
@@ -0,0 +1,64 @@
+//
+//  APIClient.swift
+//  NYC Youths
+//
+//  Created by Kartikeya Kaushal on 3/15/23.
+//
+
+import Foundation
+import Combine
+
+struct APIClient {
+    
+    var network: URLSession
+    
+    init(network: URLSession = URLSession.shared) {
+        self.network = network
+    }
+    
+    //The response information and data parsed as its data type
+    struct Response<T> {
+        let data: T
+        let response: URLResponse?
+    }
+    
+    func run<T: Decodable>(_ request: URLRequest) -> Future<T, Error> {
+        return Future { promise in
+            let task = self.network.dataTask(with: request) {data, response, error in
+                //Network call error scenario
+                if let err = error {
+                    promise(.failure(err))
+                }
+                //API Failure or unexpected response code
+                else if let res = response as? HTTPURLResponse {
+                    if res.statusCode >= 300 {
+                        let error = NSError(domain: "API Failure", code: res.statusCode, userInfo: nil)
+                        promise(.failure(error))
+                    }
+                } else {
+                    let error = NSError(domain: "Response Parse Failure", code: -1, userInfo: nil)
+                    promise(.failure(error))
+                }
+                
+                //Success
+                if let data = data {
+                    do {
+                        let decodedData = try JSONDecoder().decode(T.self, from: data)
+                        promise(.success(decodedData))
+                    } catch {
+                        let error = NSError(domain: "Data Decode Failure", code: -1, userInfo: nil)
+                        promise(.failure(error))
+                        
+                    }
+                }
+                else {
+                    let error = NSError(domain: "No Data Found", code: -1, userInfo: nil)
+                    promise(.failure(error))
+                }
+                
+                
+            }//end task definition
+            task.resume()
+        }//end Future
+    }
+}
---
+++ NYC Youths/NYC YouthsTests/MockAPIClient.swift
@@ -0,0 +1,21 @@
+//
+//  MockAPIClient.swift
+//  NYC YouthsTests
+//
+//  Created by Kartikeya Kaushal on 3/15/23.
+//
+
+@testable import NYC_Youths
+import Foundation
+
+struct MockAPIClient: APIClient {
+    var isError: Bool
+    var mockErrorMessage: String
+    
+    func run(_ request: URLRequest) -> [[String: String]] {
+        if(!isError) {
+            return [[:]] // TODO: For now we return nothing.  Eventually will improve this mock to return particular data
+        }
+        return 
+    }
+}
